f7b700186642e42b2277fc3044b437ca4e2ea8d9
norx <juan_pinillos@hotmail.com>
2026-03-16 09:52:47 -0500

    hilos y procesos

---
+++ src/Hilo.java
@@ -0,0 +1,23 @@
+public class Hilo
+{
+    public static void main(String[] args) throws InterruptedException
+    {
+        Thread t = new Thread(()  -> {
+            for (int i = 0; i < 5; i++)
+            {
+                System.out.println("Hola desde el hilo "+ Thread.currentThread().getName());
+                try
+                {
+                    Thread.sleep(3000);
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }, "worker-1");
+        t.start();
+        System.out.println("Soy el hilo principal");
+        t.join();
+    }
+}
